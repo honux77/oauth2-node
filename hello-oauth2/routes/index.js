eb64353337a3fc4bd665d4d324cc85c924d2de5f
Hoyoung Jung <honux77@gmail.com>
2020-11-16 12:56:01 +0900

    Implement Oauth2 grant
    - 환경 설정 추가 (dotenv)
    - github login url index.html에 추가

--- hello-oauth2/routes/index.js
+++ hello-oauth2/routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+require("dotenv").config();
+const express = require("express");
+const { eventNames } = require("../app");
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  const parameters = {
+    client_id: process.env.GITHUB,
+    redirect_url: process.env.HOST + process.env.REDIRECT_URL,
+    scope: process.env.SCOPE,
+  };
+  res.render("index", parameters);
 });
 
 module.exports = router;
