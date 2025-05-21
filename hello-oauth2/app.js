30ecb7ad6ebe3d619624eb7da9b4e0d6726f0dd5
Hoyoung Jung <honux77@gmail.com>
2025-05-21 18:10:39 +0900

    Update OAuth 2 Example
    state 도입
    css 포맷 가독성 향상
    코드에 주석 추가

--- hello-oauth2/app.js
+++ hello-oauth2/app.js
@@ -1,24 +1,35 @@
 require("dotenv").config();
 
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var exphbs  = require('express-handlebars');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var exphbs = require("express-handlebars");
+var session = require("express-session"); // express-session 추가
 
-var indexRouter = require('./routes/index');
+var indexRouter = require("./routes/index");
 
 var app = express();
 
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
+// 세션 설정
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "default_secret", // 환경 변수로 비밀 키 설정
+    resave: false, // 세션이 수정되지 않아도 저장 여부
+    saveUninitialized: true, // 초기화되지 않은 세션 저장 여부
+    cookie: { secure: false }, // HTTPS를 사용하는 경우 true로 설정
+  })
+);
+
+app.use("/", indexRouter);
 
 module.exports = app;
--- hello-oauth2/routes/index.js
+++ hello-oauth2/routes/index.js
@@ -1,28 +1,59 @@
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
+const crypto = require("crypto");
+const morgan = require("morgan");
 
-/* GET home page. */
+function generateState(length = 8) {
+  return crypto.randomBytes(length).toString("hex");
+}
+
+/* GitHub OAuth2 예제
+ * 1. 사용자(Resource Owner)가 링크를 클릭하여 Authorization Server에 인증 요청
+ */
 router.get("/", function (req, res, next) {
+  //generate state
+  const state = generateState();
   const parameters = {
     client_id: process.env.GH_ID,
     redirect_url: process.env.HOST + process.env.REDIRECT_URL,
     scope: process.env.SCOPE,
+    state: state,
   };
-  res.render("index", parameters);
+  console.log("state", state);
+  //state를 세션에 저장
+  req.session.state = state;
+  res.render("index", parameters); //see views/index.handlebars
 });
 
 module.exports = router;
 
+/* 2. callback URL
+ * 깃헙에서 302 리다이렉트 시켜줌
+ * auth code를 query string으로 줌
+ */
 router.get("/callback", (req, res, next) => {
   const authCode = req.query.code;
-  res.render("callback", { authCode });
+  const state = req.query.state;
+  const sessionState = req.session.state;
+  console.log("Auth Code", authCode);
+  console.log(
+    "State",
+    state,
+    sessionState,
+    "sessionState",
+    state === sessionState
+  );
+  res.render("callback", { authCode, state, sessionState });
 });
 
+/* 3. access token 요청
+ * auth code를 이용해서 access token 요청
+ * access token을 받으면 api를 호출할 수 있음
+ */
 router.get("/next", async (req, res, next) => {
   try {
-    const result = 
-    await axios.post(
+    const result = await axios.post(
       "https://github.com/login/oauth/access_token",
       {
         client_id: process.env.GH_ID,
@@ -31,25 +62,26 @@ router.get("/next", async (req, res, next) => {
       }
     );
 
-    const token = result.data.split('&')[0].split('=')[1];
-    res.render("next", {token});
+    const token = result.data.split("&")[0].split("=")[1];
+    res.render("next", { token });
   } catch (err) {
     next(err);
   }
 });
 
-//get user from github
+/* 4. access token으로 api 호출
+ * access token을 이용해서 api를 호출
+ * access token을 헤더에 넣어서 요청
+ */
 router.get("/user", async (req, res, next) => {
   const accessToken = req.query.token;
   try {
-    const result = await axios.get('https://api.github.com/user', 
-    {
-      headers: { Authorization: `Bearer ${accessToken}`}
+    const result = await axios.get("https://api.github.com/user", {
+      headers: { Authorization: `Bearer ${accessToken}` },
     });
-
     const data = result.data;
-    res.json({accessToken, data});
-
+    apiURL = "https://api.github.com/user";
+    res.json({ accessToken, apiURL, data });
   } catch (err) {
     next(err);
   }
